070ffc265400347005a6fa10c1bde4f50441eb36
Shlomo Hailo <shlomoohailo@gmail.com>
2022-05-22 23:47:38 +0300

    coomit

---
+++ main.js
@@ -0,0 +1,93 @@
+// function js_string_3_b(str) {
+//     var obj = {
+//         city: "haifa"
+//     }
+//     return obj[str];
+// }
+// js_string_3_b(city);
+
+
+// function getString(str) {
+//     if (str.length > 5) {
+//         return "long";
+//     }
+//     else {
+//         return "short";
+//     }
+// }
+// getString("shlomoo");
+
+// function getStringA(str) {
+//     for (var i = 0; i < str.length; i++) {
+//         if (str[i] == "a") {
+//             return (i);
+//         }
+//         else {
+//             return "not found";
+//         }
+//     }
+
+// }
+// getStringA("shloomo hilo");
+
+
+// function getStringR(str) {
+//     for (var i = 0; i < str.length; i++) {
+//         if (str[i] == "r"){
+//             return "true";
+//         }
+//         else{
+//             return "false";
+//         }
+
+//     }
+
+// }
+// getStringR("rizhon");
+
+// function getStringAndTav(str, tav) {
+//     for (var i = 0; i < str.length; i++) {
+//         if (str[i] == tav) {
+//             return "true";
+//         }
+//         else {
+//             return "false";
+//         }
+
+//     }
+// }
+// getStringAndTav("shlomo", "h");
+
+// function terurenTreeTav() {
+//     var str = "sayonara";
+//     var getString = str.substr(0, 3);
+//     return getString;
+// }
+// terurenTreeTav();
+
+
+// function terurenEndTav() {
+//     var str = "sayonara";
+//     var getString = str.substr(2);
+//     return getString;
+// }
+// terurenEndTav();
+
+
+// function getStringAndIndex(str, index) {
+//     if (index > str.length) {
+//         return "big";
+//     }
+//     else {
+//         return str.substr(index);
+//     }
+// }
+// getStringAndIndex("shlomo", 4);
+
+
+// function getTwoString(str1, str2) {
+//     if (str1.length == str2.length){
+//         return str1 ;
+//     }
+// }
+// getTwoString("shlomo", "hailoo");
